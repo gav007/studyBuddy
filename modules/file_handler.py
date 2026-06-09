ed8978ab9686ccb3933b17afb7bfbe7862f64f0a
Gavin May <gav.s.may@gmail.com>
2026-06-09 14:27:25 +0100

    Display function now displays table on console

--- modules/file_handler.py
+++ modules/file_handler.py
@@ -38,7 +38,26 @@ def read_data(file_path):
         
 
 def display_table(header, rows):
-    print(header)
+    
+    print("STUDY BUDDY")
+    print("-" * 80)
 
+    date = header[0].ljust(15)
+    subject = header[1].ljust(15)
+    topic = header[2].ljust(25)
+    minutes = header[3].ljust(12)
+    confidence = header[4].ljust(12)
+
+    header_display = date + subject + topic + minutes + confidence
+    print(header_display)
+    print("-" * 80)
+    
     for row in rows:
-        print(row)
+        row_date = row[0].ljust(15)
+        row_subject = row[1].ljust(15)
+        row_topic = row[2].ljust(25)
+        row_minutes = row[3].ljust(12)
+        row_confidence = row[4].ljust(12)
+        
+        row_display = row_date + row_subject + row_topic + row_minutes + row_confidence
+        print(row_display)
