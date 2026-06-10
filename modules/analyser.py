5089e12ccf6e6e921807ec77b432cd4a89b45cd1
Gavin May <gav.s.may@gmail.com>
2026-06-10 20:38:08 +0100

    Add minutes by subject analyser

--- modules/analyser.py
+++ modules/analyser.py
@@ -17,3 +17,23 @@ def calculate_total_minutes(rows, header):
         total_minutes += minutes
 
     return  total_minutes
+
+
+def calculate_minutes_by_subject(rows, header):
+    
+    get_subject_index = header.index("Subject")
+    get_minutes_index = header.index("Minutes")
+    
+    # creates a diction which will contain the suject as a key and minutes as a value
+    minutes_by_subject = {}
+    
+    for row in rows:
+        subject = row[get_subject_index]
+        minutes = int(row[get_minutes_index])
+        
+        if subject in minutes_by_subject:
+            minutes_by_subject[subject] = minutes_by_subject[subject] + minutes
+        else:
+            minutes_by_subject[subject] = minutes
+            
+    return minutes_by_subject
